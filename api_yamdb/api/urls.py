bba8c830923f85e5d8600202e3781ea1c4edc4e4
Janejuly1 <janejuly1@gmail.com>
2021-12-02 21:09:19 +0300

    small optimizations

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -15,7 +15,7 @@ v1_router.register(r'titles/(?P<title_id>\d+)/reviews',
                    ReviewViewSet, basename='reviews')
 v1_router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
                    CommentViewSet, basename='comments')
-v1_router.register('users', UserViewSet, basename='users')
+v1_router.register('users', UserViewSet)
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
